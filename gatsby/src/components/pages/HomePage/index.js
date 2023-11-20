fa868d345197022b10714d90d5e9e465eceb2e87
AdrienLapasset <aflapasset@gmail.com>
2023-11-20 14:59:03 +0100

    add page animation

--- gatsby/src/components/pages/HomePage/index.js
+++ gatsby/src/components/pages/HomePage/index.js
@@ -1,17 +1,41 @@
 import * as React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import PostsSection from "./PostsSection";
+import styled from "styled-components";
+import { Link } from "@reach/router";
 
-const HomePage = () => {
+const StyledContainer = styled.div`
+  background-color: white;
+  display: flex;
+  transition: flex 1s;
+  flex: 0 0 ${({ isActive }) => (isActive ? 90 : 10)}vw;
+`;
+const StyledNavigation = styled.div`
+  writing-mode: sideways-lr;
+  text-orientation: upright;
+  text-align: end;
+  display: block;
+  padding: 15px;
+`;
+const StyledPageContent = styled.div``;
+
+const HomePage = ({ currentPath }) => {
+  const isActive = currentPath === "/";
+
   return (
-    <div className="pageAnimation">
-      <h1>HomePage</h1>
-      <StaticImage
-        src="../../../assets/imgs/homeHeroImage.jpg"
-        alt="TLMR - L’excellence accessible"
-      />
-      <PostsSection />
-    </div>
+    <StyledContainer isActive={isActive}>
+      <StyledNavigation>
+        <Link to="/">La Croix Gratiot</Link>
+      </StyledNavigation>
+      <StyledPageContent>
+        <h1>HomePage</h1>
+        <StaticImage
+          src="../../../assets/imgs/homeHeroImage.jpg"
+          alt="TLMR - L’excellence accessible"
+        />
+        <PostsSection />
+      </StyledPageContent>
+    </StyledContainer>
   );
 };
 
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -3,53 +3,117 @@ import Layout from "components/Layout";
 import HomePage from "components/pages/HomePage";
 import NosPratiquesPage from "components/pages/NosPratiques";
 import styled from "styled-components";
-import { navigate } from "@reach/router";
+import { Link } from "@reach/router";
+import { useLocation } from "@reach/router";
+import { StaticImage } from "gatsby-plugin-image";
 
 const StyledContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  display: flex;
+  overflow: hidden;
+  .page {
+  }
 `;
-const StyledLink = styled.div`
+
+const StyledPage = styled.div`
+  display: flex;
+  transition: left 1s;
+  position: absolute;
+  background-color: white;
+  width: 100%;
+  &:nth-child(2) {
+    left: ${({ isActive }) => (isActive ? "50px" : "calc(100vw - 100px)")};
+    z-index: 1;
+  }
+  &:nth-child(3) {
+    left: ${({ isActive }) => (isActive ? "100px" : "calc(100vw - 50px)")};
+    z-index: 2;
+  }
+  .pageContent {
+    height: 100vh;
+    transition: opacity 1s;
+  }
+`;
+
+const StyledLink = styled(Link)`
   writing-mode: sideways-lr;
   text-orientation: upright;
   text-align: end;
   padding: 15px;
-  display: none;
-  @media ${(props) => props.theme.minWidth.md} {
-    display: block;
-  }
-
-  &.homePage {
-    transition: all 1s;
-    transform: translateX(
-      ${({ isNosPratiques }) => (isNosPratiques ? -1000 : 0)}px
-    );
-  }
-  &.nosPratiques {
-    transition: all 1s;
-    transform: translateX(
-      ${({ isNosPratiques }) => (isNosPratiques ? -1000 : 0)}px
-    );
-  }
 `;
 
+const Page = ({ page }) => {
+  const { pathname } = useLocation();
+
+  const isNosPratiquesActive =
+    pathname === "/nos-pratiques/" || pathname === "/la-degustation/";
+  const isLadegustationActive = pathname === "/la-degustation/";
+
+  if (page === "Home") {
+    return (
+      <StyledPage>
+        <StyledLink to="/">Home</StyledLink>
+        <div className="pageContent">
+          <h1>Home</h1>
+          <StaticImage
+            src="../assets/imgs/homeHeroImage.jpg"
+            alt="TLMR - L’excellence accessible"
+          />
+        </div>
+      </StyledPage>
+    );
+  }
+  if (page === "Nos Pratiques") {
+    return (
+      <StyledPage isActive={isNosPratiquesActive}>
+        <StyledLink to="/nos-pratiques/">Nos Pratiques</StyledLink>
+        <div className="pageContent">
+          <h1>Nos Pratiques</h1>
+          <StaticImage
+            src="../assets/imgs/homeHeroImage.jpg"
+            alt="TLMR - L’excellence accessible"
+          />
+        </div>
+      </StyledPage>
+    );
+  }
+  if (page === "La Dégustation") {
+    return (
+      <StyledPage isActive={isLadegustationActive}>
+        <StyledLink to="/la-degustation/">La Dégusatation</StyledLink>
+        <div className="pageContent">
+          <h1>La Dégusatation</h1>
+          <StaticImage
+            src="../assets/imgs/homeHeroImage.jpg"
+            alt="TLMR - L’excellence accessible"
+          />
+        </div>
+      </StyledPage>
+    );
+  }
+};
+
 const Index = () => {
-  const [isNosPratiques, setIsNosPratiques] = useState(false);
-
-  const handleClick = () => {
-    setIsNosPratiques(!isNosPratiques);
-    navigate("/nos-pratiques/");
-  };
-
   return (
     <Layout>
       <StyledContainer>
-        <StyledLink onClick={handleClick}>La Croix Gratiot</StyledLink>
-        <HomePage className="homePage" />
-        <NosPratiquesPage className="nosPratiques" />
-        <StyledLink isNosPratiques={isNosPratiques} onClick={handleClick}>
-          Nos Pratiques
-        </StyledLink>
+        <Page page="Home" />
+        <Page page="Nos Pratiques" />
+        <Page page="La Dégustation" />
+        {/* {pages.map(({ title, path }) => {
+          const isActive = path === pathname;
+          return (
+            <StyledPage key={title} $isActive={isActive}>
+              <StyledLink to={path}>{title}</StyledLink>
+              <div className="pageContent">
+                <h1>{title}</h1>
+                <StaticImage
+                  src="../assets/imgs/homeHeroImage.jpg"
+                  alt="TLMR - L’excellence accessible"
+                />
+              </div>
+            </StyledPage>
+          );
+        })} */}
       </StyledContainer>
     </Layout>
   );
