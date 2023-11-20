b1408926bb0a7d49ad9a345a2a6fa595f0690884
AdrienLapasset <aflapasset@gmail.com>
2023-11-20 16:37:11 +0100

    init page navigation

--- gatsby/src/components/pages/LaDegustationPage/index.js
+++ gatsby/src/components/pages/LaDegustationPage/index.js
@@ -2,15 +2,17 @@ import * as React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import styled from "styled-components";
 
-const StyledContainer = styled.div``;
+const StyledContainer = styled.div`
+  background-color: white;
+`;
 
-const NosPratiques = () => {
+const HomePage = () => {
   return (
     <StyledContainer>
-      <h1>Nos Pratiques</h1>
+      <h1>La Degustation</h1>
       <StaticImage src="../../../assets/imgs/homeHeroImage.jpg" alt="" />
     </StyledContainer>
   );
 };
 
-export default NosPratiques;
+export default HomePage;
