81c844c0cf7ed4636a1b2b737fa3ce93f4cf1659
AdrienLapasset <aflapasset@gmail.com>
2023-12-04 14:57:51 +0100

    add page transition

--- gatsby/src/components/Layout/DesktopLayout.js
+++ gatsby/src/components/Layout/DesktopLayout.js
@@ -4,146 +4,42 @@ import NosPratiquesPage from "components/pages/NosPratiques";
 import LaDegustationPage from "components/pages/LaDegustation";
 import NousRencontrerPage from "components/pages/NousRencontrer";
 import styled from "styled-components";
-import { Router, Link, useLocation } from "@reach/router";
+import { Router, Link, useLocation, navigate } from "@reach/router";
 import LogoLGCDesktop from "assets/logos/LogoLGCDesktop";
-
-// const StyledPageContainer = styled.div`
-//   transition: left 1s;
-//   position: absolute;
-//   width: 100%;
-//   display: flex;
-//   &:nth-child(2) {
-//     left: ${({ $isActive }) => ($isActive ? "60px" : "calc(100vw - 180px)")};
-//     z-index: 1;
-//   }
-//   &:nth-child(3) {
-//     left: ${({ $isActive }) => ($isActive ? "120px" : "calc(100vw - 120px)")};
-//     z-index: 2;
-//   }
-//   &:nth-child(4) {
-//     left: ${({ $isActive }) => ($isActive ? "180px" : "calc(100vw - 60px)")};
-//     z-index: 3;
-//   }
-//   .pageContent {
-//     background-color: ${({ theme }) => theme.colors.backgroundLight};
-//     @media ${({ theme }) => theme.minWidth.xl} {
-//       width: calc(100% - 270px);
-//     }
-//     &:not(.home) {
-//       @media ${({ theme }) => theme.minWidth.xl} {
-//         width: calc(100% - 270px);
-//         padding: 0 180px 0 60px;
-//       }
-//     }
-//   }
-// `;
-// const StyledLinkContainer = styled.div`
-//   flex: 0 0 60px;
-//   border-left: 2px solid
-//     ${({ theme, $activeSection }) =>
-//       $activeSection === "orange"
-//         ? theme.colors.black
-//         : $activeSection === "red" || $activeSection === "green"
-//         ? theme.colors.backgroundLight
-//         : theme.colors.black};
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   transition: all 1s;
-//   background-color: ${({ theme, $activeSection }) =>
-//     $activeSection === "orange"
-//       ? theme.colors.orange
-//       : $activeSection === "green"
-//       ? theme.colors.green
-//       : $activeSection === "red"
-//       ? theme.colors.red
-//       : $activeSection === "white"
-//       ? theme.colors.backgroundLight
-//       : theme.colors.backgroundLight};
-//   a,
-//   button {
-//     font-size: 18px;
-//     text-transform: uppercase;
-//     letter-spacing: 0.9px;
-//     writing-mode: sideways-lr;
-//     text-orientation: upright;
-//     text-align: end;
-//     transition: color 1s;
-//     color: ${({ theme, $activeSection }) =>
-//       $activeSection === "orange"
-//         ? theme.colors.black
-//         : $activeSection === "red" || $activeSection === "green"
-//         ? theme.colors.backgroundLight
-//         : theme.colors.black};
-//   }
-//   &:not(.homeNav) {
-//     position: fixed;
-//     min-height: 100vh;
-//     width: 60px;
-//     top: 0;
-//     padding-top: 30px;
-//   }
-//   &.homeNav {
-//     border: none;
-//     z-index: 1;
-//     & > div {
-//       display: flex;
-//       flex-direction: column;
-//       justify-content: space-between;
-//       align-items: center;
-//       height: calc(100vh - 100px);
-//       position: fixed;
-//       & > div {
-//         width: 100%;
-//         display: flex;
-//         flex-direction: column;
-//         button {
-//           margin-top: 15px;
-//         }
-//       }
-//     }
-//     svg {
-//       path {
-//         transition: fill 1s;
-//         fill: ${({ theme, $activeSection }) =>
-//           $activeSection === "orange"
-//             ? theme.colors.black
-//             : $activeSection === "red" || $activeSection === "green"
-//             ? theme.colors.backgroundLight
-//             : theme.colors.black};
-//       }
-//     }
-//   }
-// `;
 
 const StyledContainer = styled.div`
   nav {
+    /* z-index: -1; */
     position: fixed;
-    background-color: white;
+    background-color: ${({ theme }) => theme.colors.backgroundLight};
     display: grid;
     top: 0;
     bottom: 0;
     width: 100%;
     transition: all 1s;
-    grid-template-columns: ${({ $activePage }) =>
-      $activePage === "/"
+    grid-template-columns: ${({ $incomingPage }) =>
+      $incomingPage === "/"
         ? "calc(100vw - 180px) 60px 60px 60px"
-        : $activePage === "/nos-pratiques/"
+        : $incomingPage === "/nos-pratiques/"
         ? "60px calc(100vw - 180px) 60px 60px"
-        : $activePage === "/la-degustation/"
-        ? "60px 60px calc(100vw - 180px)  60px"
-        : $activePage === "/nous-rencontrer/"
+        : $incomingPage === "/la-degustation/"
+        ? "incomingPage 60px calc(100vw - 180px)  60px"
+        : $incomingPage === "/nous-rencontrer/"
         ? "60px 60px 60px calc(100vw - 180px)"
         : null};
-    a {
+    & > div {
+      display: block;
       border-left: 2px solid;
-      font-size: 18px;
-      text-transform: uppercase;
-      letter-spacing: 0.9px;
-      writing-mode: sideways-lr;
-      text-orientation: upright;
-      text-align: end;
       padding: 30px 0 0 15px;
+      button {
+        writing-mode: sideways-lr;
+        letter-spacing: 0.9px;
+        text-transform: uppercase;
+        font-size: 18px;
+        text-align: end;
+        text-orientation: upright;
+        background-color: ${({ theme }) => theme.colors.backgroundLight};
+      }
     }
   }
   main {
@@ -157,6 +53,35 @@ const StyledContainer = styled.div`
         : $activePage === "/nous-rencontrer/"
         ? "0 0 0 240px"
         : null};
+    transition: opacity 1s;
+    opacity: ${({ $isTransition }) => ($isTransition ? 0 : 1)};
+    .transitionMask {
+      position: absolute;
+      background-color: ${({ theme }) => theme.colors.backgroundLight};
+      height: 100vh;
+      top: 0;
+      transition: all 1s;
+      left: ${({ $activePage, $isTransition }) =>
+        $isTransition && $activePage === "/"
+          ? "60px"
+          : $isTransition && $activePage === "/nos-pratiques/"
+          ? "120px"
+          : $isTransition && $activePage === "/la-degustation/"
+          ? "180px"
+          : $isTransition && $activePage === "/nous-rencontrer/"
+          ? "240px"
+          : "100vw"};
+      right: ${({ $activePage, $isTransition }) =>
+        $activePage === "/"
+          ? "180px"
+          : $activePage === "/nos-pratiques/"
+          ? "120px"
+          : $activePage === "/la-degustation/"
+          ? "60px"
+          : $activePage === "/nous-rencontrer/"
+          ? "0"
+          : null};
+    }
   }
 `;
 
@@ -164,20 +89,46 @@ export default function DesktopLayout() {
   const { pathname } = useLocation();
 
   const [activePage, setActivePage] = useState(pathname);
+  const [incomingPage, setIncomingPage] = useState(pathname);
+  const [isTransition, setIsTransition] = useState(false);
 
-  useEffect(() => {
-    setActivePage(pathname);
-  }, [pathname]);
+  function handlePageChange(page) {
+    setIncomingPage(page);
+    setIsTransition(true);
+    setTimeout(() => {
+      navigate(page);
+      setActivePage(page);
+      setIsTransition(false);
+    }, 1000);
+  }
 
   return (
-    <StyledContainer $activePage={activePage}>
+    <StyledContainer
+      $incomingPage={incomingPage}
+      $activePage={activePage}
+      $isTransition={isTransition}
+    >
       <nav>
-        <Link to="/">
-          <LogoLGCDesktop />
-        </Link>
-        <Link to="/nos-pratiques/">Nos Pratiques</Link>
-        <Link to="/la-degustation/">La Dégustation</Link>
-        <Link to="/nous-rencontrer/">Nous rencontrer</Link>
+        <div>
+          <button onClick={() => handlePageChange("/")}>
+            <LogoLGCDesktop />
+          </button>
+        </div>
+        <div>
+          <button onClick={() => handlePageChange("/nos-pratiques/")}>
+            Nos Pratiques
+          </button>
+        </div>
+        <div>
+          <button onClick={() => handlePageChange("/la-degustation/")}>
+            La Dégustation
+          </button>
+        </div>
+        <div>
+          <button onClick={() => handlePageChange("/nous-rencontrer/")}>
+            Nous rencontrer
+          </button>
+        </div>
       </nav>
       <main>
         <Router>
@@ -186,61 +137,8 @@ export default function DesktopLayout() {
           <LaDegustationPage path="/la-degustation/" />
           <NousRencontrerPage path="/nous-rencontrer/" />
         </Router>
+        <div className="transitionMask"></div>
       </main>
     </StyledContainer>
-
-    // <StyledContainer $pageHeight={pageHeight}>
-    //   {/* HomePage */}
-    //   <StyledPageContainer ref={homePageRef}>
-    //     <StyledLinkContainer
-    //       className="homeNav"
-    //       $activeSection={activeHomeSection}
-    //     >
-    //       <div>
-    //         <Link to="/">
-    //           <LogoLGCDesktop />
-    //         </Link>
-    //         <div>
-    //           <button>EN</button>
-    //           <button>FR</button>
-    //         </div>
-    //       </div>
-    //     </StyledLinkContainer>
-    //     <HomePage className="pageContent home" />
-    //   </StyledPageContainer>
-
-    //   {/* Nos Pratiques */}
-    //   <StyledPageContainer
-    //     $isActive={isNosPratiquesActive}
-    //     ref={nosPratiquesRef}
-    //   >
-    //     <StyledLinkContainer $activeSection={activeHomeSection}>
-    //       <Link to="/nos-pratiques/">Nos Pratiques</Link>
-    //     </StyledLinkContainer>
-    //     <NosPratiquesPage className="pageContent" />
-    //   </StyledPageContainer>
-
-    //   {/* La Dégustation */}
-    //   <StyledPageContainer
-    //     $isActive={isLadegustationActive}
-    //     ref={laDegustationRef}
-    //   >
-    //     <StyledLinkContainer $activeSection={activeHomeSection}>
-    //       <Link to="/la-degustation/">La Dégustation</Link>
-    //     </StyledLinkContainer>
-    //     <LaDegustationPage className="pageContent" />
-    //   </StyledPageContainer>
-
-    //   {/* Nous rencontrer */}
-    //   <StyledPageContainer
-    //     $isActive={isNousRencontrerActive}
-    //     ref={nousRencontrerRef}
-    //   >
-    //     <StyledLinkContainer $activeSection={activeHomeSection}>
-    //       <Link to="/nous-rencontrer/">Nous rencontrer</Link>
-    //     </StyledLinkContainer>
-    //     <NousRencontrerPage className="pageContent" />
-    //   </StyledPageContainer>
-    // </StyledContainer>
   );
 }
