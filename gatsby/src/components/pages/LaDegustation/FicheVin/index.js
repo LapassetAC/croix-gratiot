d06386dea9f74569ee4b22f089b909bde655842a
Clément Lapasset <clement.lapasset@gmail.com>
2023-12-06 17:40:34 +0100

    add all info in wine schema

--- gatsby/src/components/pages/LaDegustation/FicheVin/index.js
+++ gatsby/src/components/pages/LaDegustation/FicheVin/index.js
@@ -14,7 +14,6 @@ const StyledContainer = styled.div`
 `;
 
 const FicheVin = ({
-  slug,
   title,
   category,
   cepages,
--- sanity/schemas/wine.js
+++ sanity/schemas/wine.js
@@ -13,6 +13,8 @@ export default {
       name: 'slug',
       type: 'slug',
       title: 'URL de la page',
+      description:
+        "Le bouton 'Generate' génère automatiquement l'URL de la page du vin. Cet URL est modifiable.",
       validation: (Rule) => Rule.required(),
       options: {
         source: 'title',
@@ -42,7 +44,7 @@ export default {
     },
     {
       name: 'portraitImage',
-      title: 'Image portrait',
+      title: 'Image portrait du vin',
       description: 'Image de la bouteille mise en scène (format portrait)',
       type: 'image',
       validation: (Rule) => Rule.required(),
@@ -55,9 +57,91 @@ export default {
     },
     {
       name: 'cepages',
-      title: 'Cépages',
+      title: 'Cépage(s)',
       type: 'string',
       validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'quote',
+      title: 'Citation',
+      description:
+        'Citation qui apparaît sous le titre du vin. Ex. pour le rosé : Osez Rosé, Roséphine !',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'adjectif',
+      title: 'Le vin en un mot',
+      description: 'Description en un mot du vin. Ex. pour le rosé : Le Velouté',
+      type: 'string',
+    },
+    {
+      name: 'intro',
+      title: 'Le vin en deux phrases',
+      description:
+        'Description en deux phrases du vin. Ex. pour le rosé : Velouté Syrah et Grenache. Ce vin très expressif, gourmand et fruité accompagné d’une belle vivacité.',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'description',
+      title: 'Le mot des vignerons',
+      description: 'Description du vin, de son origine, son histoire, etc.',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'videoUrl',
+      title: 'URL vidéo (optionnel)',
+      type: 'url',
+      description: 'URL complet de la vidéo',
+      validation: (Rule) =>
+        Rule.uri({
+          allowRelative: false,
+          scheme: ['http', 'https'],
+        }),
+    },
+    {
+      name: 'vinification',
+      title: 'Vinification (optionnel)',
+      description: 'Description de la vinification',
+      type: 'text',
+    },
+    {
+      name: 'degustation',
+      title: 'Dégustation (optionnel)',
+      description: 'Description de la dégustation du vin, ses associations, etc.',
+      type: 'text',
+    },
+    {
+      name: 'landscapeImage',
+      title: 'Image paysage du vin (optionnel)',
+      description: 'Image de la bouteille mise en scène (format paysage)',
+      type: 'image',
+    },
+    {
+      name: 'alcool',
+      title: 'Teneur en alcool',
+      description: 'Ne fournir que le nombre. Ex. : 12 pour 12% Vol.',
+      type: 'number',
+    },
+    {
+      name: 'millesime',
+      title: 'Millésime',
+      type: 'string',
+    },
+    {
+      name: 'formats',
+      title: 'Formats disponibles',
+      type: 'array',
+      of: [{type: 'string'}],
+      options: {
+        list: [
+          {title: 'Demi (37,5cl)', value: 'demi'},
+          {title: 'Bouteille (75cl)', value: 'bouteille'},
+          {title: 'Magnum (1,5l)  ', value: 'magnum'},
+        ],
+      },
     },
   ],
 }
